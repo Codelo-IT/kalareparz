f6999827857f6580a63b2918ec4e006a31e0b0e3
Oskar Jaworski <oskar.jaworski@weastronauts.com>
2023-11-14 23:02:33 +0100

    <ContentWrapper/>: Change name

--- src/components/ContentWrapper.tsx
+++ src/components/ContentWrapper.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from "react";
 
-export const Content = ({ children }: PropsWithChildren) =>
+export const ContentWrapper = ({ children }: PropsWithChildren) =>
 	<div className="max-w-[1440px] mx-auto px-8">
 		{ children }
 	</div>
