5fb7ab4524645163b91cb5e586db9b1c1785776e
Oskar Jaworski <oskar.jaworski@weastronauts.com>
2023-11-14 22:24:42 +0100

    Header: Increase shadow

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "assets/logo.png";
 
 export const Header = () => (
-	<div className="h-[100px] shadow w-screen flex items-center fixed z-50 justify-center text-xl bg-white">
+	<div className="h-[100px] shadow-md w-screen flex items-center fixed z-50 justify-center text-xl bg-white">
 		<a href="/#">
 			<img
 				src={ logo }
@@ -10,6 +10,9 @@ export const Header = () => (
 				className="absolute top-5 left-5 h-36"
 			/>
 		</a>
+		<div className="font-garamond text-6xl mr-52 italic font-medium">
+			Kalareparz
+		</div>
 		<div className="flex items-center gap-20">
 			<a href="/#wystawcy" className="cursor-pointer">
 				Wystawcy
