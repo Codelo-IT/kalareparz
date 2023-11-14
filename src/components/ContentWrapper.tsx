bea2efde5d6a7b821b77440a9019000b330da9cc
Oskar Jaworski <oskar.jaworski@weastronauts.com>
2023-11-14 23:05:52 +0100

    <ContentWrapper/>: Add className prop

--- src/components/ContentWrapper.tsx
+++ src/components/ContentWrapper.tsx
@@ -1,6 +1,14 @@
 import { PropsWithChildren } from "react";
+import classNames from "classnames";
 
-export const ContentWrapper = ({ children }: PropsWithChildren) =>
-	<div className="max-w-[1440px] mx-auto px-8">
+type Props = {
+	className?: string
+}
+
+export const ContentWrapper = ({
+	children,
+	className,
+}: PropsWithChildren<Props>) =>
+	<div className={ classNames("max-w-[1440px] mx-auto px-8", className) }>
 		{ children }
 	</div>
