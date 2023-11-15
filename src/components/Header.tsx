b4f088d7ec4e05877d8619188a669e777efc8e18
Oliwia Putyra <oliwia.putyra02@gmail.com>
2023-11-15 22:59:23 +0100

    Header: Add background when scrolled down

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Logo } from "components/Logo";
+import classNames from "classnames";
 
-export const Header = () => (
-	<header className="py-10 w-screen flex items-center fixed z-50 justify-center text-xl">
-		<Logo className="absolute top-4 left-5 h-20"/>
-		<div className="relative flex flex-col justify-center items-center gap-5">
-			<div className="flex items-center gap-20">
-				<a href="/#onas" className="cursor-pointer transition hover:text-green tracking-wide">
-					O nas
-				</a>
-				<a href="/#wystawcy" className="cursor-pointer transition hover:text-green tracking-wide">
-					Wystawcy
-				</a>
-				<a href="/#kontakt" className="cursor-pointer transition hover:text-green tracking-wide">
-					Kontakt
-				</a>
+export const Header = () => {
+	const [ scrollPosition, setScrollPosition ] = useState<number>(0);
+
+	useEffect(() => {
+		window.addEventListener('scroll', () => {
+			setScrollPosition(window.scrollY);
+		})
+	}, []);
+
+	return (
+		<header className={
+			classNames(
+				"w-screen fixed flex py-10 items-center z-50 justify-center text-xl transition duration-200",
+				{"bg-white shadow-md": scrollPosition != 0}
+			)
+		}>
+			<Logo className="absolute top-4 left-5 h-20"/>
+			<div className="relative flex flex-col justify-center items-center gap-5">
+				<div className="flex items-center gap-20">
+					<a href="/#onas" className="cursor-pointer transition hover:text-green tracking-wide">
+						O nas
+					</a>
+					<a href="/#wystawcy" className="cursor-pointer transition hover:text-green tracking-wide">
+						Wystawcy
+					</a>
+					<a href="/#kontakt" className="cursor-pointer transition hover:text-green tracking-wide">
+						Kontakt
+					</a>
+				</div>
 			</div>
-		</div>
-	</header>
-)
+		</header>
+	);
+}
