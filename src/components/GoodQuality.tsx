afc8e7b899a210b071a7c8822f0e169255bfa954
Oliwia Putyra <oliwia.putyra02@gmail.com>
2023-11-16 01:36:54 +0100

    Good Quality: Fix styles

--- src/components/GoodQuality.tsx
+++ src/components/GoodQuality.tsx
@@ -2,13 +2,13 @@ import laurelWreathLeft from "assets/laurel-wreath-left.svg";
 import laurelWreathRight from "assets/laurel-wreath-right.svg";
 
 export const GoodQuality = () => (
-	<section className="w-full h-[250px] flex flex-col justify-center items-center bg-green">
+	<section className="w-full h-[150px] flex flex-col justify-center items-center bg-green">
 		<div className="flex justify-center items-center">
-			<img src={ laurelWreathLeft } className="h-[150px]" alt="Laurel Wreath"/>
-			<div className="text-5xl font-bold text-center max-w-[600px] text-white">
+			<img src={ laurelWreathLeft } className="h-[100px]" alt="Laurel Wreath"/>
+			<div className="text-3xl font-bold text-center max-w-[600px] text-white font-title">
 				Zasmakuj lokalnej pasji!
 			</div>
-			<img src={ laurelWreathRight } className="h-[150px]" alt="Laurel Wreath"/>
+			<img src={ laurelWreathRight } className="h-[100px]" alt="Laurel Wreath"/>
 		</div>
 	</section>
 );
