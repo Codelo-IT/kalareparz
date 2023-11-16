003c8ba7810b7e446ba2dcab28d46f1f0f7d3af0
Oliwia Putyra <oliwia.putyra02@gmail.com>
2023-11-16 02:31:04 +0100

    Contact: RWD

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -11,31 +11,35 @@ export const Contact = () => {
 	const [ isSidebarOpen, toggleSidebarOpen ] = useState(true);
 
 	return (
-		<section id="kontakt" className="container-wrapper relative mt-20 h-[500px] rounded-2xl overflow-hidden">
-			<div
-				className="absolute top-0 w-[600px] h-full flex flex-col gap-5 p-10 z-20 rounded-l-2xl rounded-b-2xl bg-light transition-all"
-				style={ { left: isSidebarOpen ? 0 : -568 } }
-			>
+		<section id="kontakt" className="container-wrapper mt-20">
+			<div className="rounded-2xl overflow-hidden relative xl:h-[500px]">
 				<div
-					className="absolute top-5 -right-8 bg-light p-2 rounded-r cursor-pointer"
-					onClick={ () => toggleSidebarOpen(isOpen => !isOpen) }
+					className="xl:absolute top-0 xl:w-[600px] xl:h-full flex flex-col gap-5 p-6 xl:p-10 z-20 rounded-l-2xl bg-light transition-all"
+					style={ { left: isSidebarOpen ? 0 : -600 } }
 				>
-					<img src={ hamburgerIcon } alt="<-" className="h-5"/>
+					<div
+						className="hidden xl:block absolute top-5 -right-8 bg-light p-2 rounded-r cursor-pointer"
+						onClick={ () => toggleSidebarOpen(isOpen => !isOpen) }
+					>
+						<img src={ hamburgerIcon } alt="<-" className="h-5"/>
+					</div>
+					<div className="mb-2 text-3xl lg:text-4xl xl:text-5xl text-black font-semibold font-title">
+						Odwiedź Kalareparz
+					</div>
+					<div className="text-gray-500 font-light text-base xl:text-lg">
+						Jeśli zależy Ci na zdrowiu Twoim i Twojej rodziny, odwiedź nasz targ. Świeże warzywa i owoce, soki, przetwory, wędliny - na wyciągnięcie ręki!
+					</div>
+					<div className="text-black font-semibold text-base xl:text-lg mt-5">
+						Będziemy na Ciebie czekać:
+					</div>
+					<ContactItem icon={ calendarIcon } label="Co sobotę"/>
+					<ContactItem icon={ clockIcon } label="Od 8:00 do 13:00"/>
+					<ContactItem icon={ locationIcon } label="Na ul. Kazimierza Wielkiego 10 w Skawinie"/>
 				</div>
-				<div className="mb-2 text-5xl text-black font-semibold font-title">
-					Odwiedź Kalareparz
+				<div className="h-[300px] xl:h-full">
+					<Map/>
 				</div>
-				<div className="text-gray-500 font-light text-lg">
-					Jeśli zależy Ci na zdrowiu Twoim i Twojej rodziny, odwiedź nasz targ. Świeże warzywa i owoce, soki, przetwory, wędliny - na wyciągnięcie ręki!
-				</div>
-				<div className="text-black font-semibold text-lg mt-5">
-					Będziemy na Ciebie czekać:
-				</div>
-				<ContactItem icon={ calendarIcon } label="Co sobotę"/>
-				<ContactItem icon={ clockIcon } label="Od 8:00 do 13:00"/>
-				<ContactItem icon={ locationIcon } label="Na ul. Kazimierza Wielkiego 10 w Skawinie"/>
 			</div>
-			<Map/>
 		</section>
 	);
 };
--- src/components/ContactItem.tsx
+++ src/components/ContactItem.tsx
@@ -12,16 +12,16 @@ export const ContactItem = ({ link, icon, label }: Props) =>
 	<>
 		{
 			isNotNull(link) ?
-				<Link className="flex items-center gap-5" to={ link }>
+				<Link className="flex items-center gap-3 xl:gap-5" to={ link }>
 					<img src={ icon } alt="calendarIcon" className="h-8"/>
-					<div className="text-lg text-black font-light">
+					<div className="text-base xl:text-lg text-black font-light">
 						{ label }
 					</div>
 				</Link>
 				:
-				<div className="flex items-center gap-5">
+				<div className="flex items-center flex-col sm:flex-row gap-3 xl:gap-5">
 					<img src={ icon } alt="calendarIcon" className="h-8"/>
-					<div className="text-lg font-medium">
+					<div className="text-base text-center sm:text-left xl:text-lg font-medium">
 						{ label }
 					</div>
 				</div>
